f461c7d8de1ae2adab60eef337137cf592e41649
Ahmadreza Za <159421656+Ahmadre01@users.noreply.github.com>
2024-02-08 13:09:45 +0330

    Update Creates a random story with Using words selected from different lists.py

--- Creates a random story with Using words selected from different lists.py
+++ Creates a random story with Using words selected from different lists.py
@@ -5,12 +5,6 @@ verbs = [ "reads" , "eats" , "smells" , "drinks" , "grabs" ]
 print ( "\n  SENTENCE ==> The" ,random.choice(adjectives),random.choice(nouns),
        random.choice(verbs), ", the" ,
        random.choice(adjectives),random.choice(nouns),"\n")
-# یک داستان تصادفی ایجاد می‌کند با استفاده از کلمات انتخابی از لیست‌های مختلف
-# ابتدا سه لیست از کلمات تعریف شده‌اند: 
-# اسم‌ها (nouns)، صفت‌ها (adjectives) و فعل‌ها (verbs). 
-# random.choice()با استفاده از تابع 
-# در پایتون استفاده میکندrandom که از کتابخانه 
-# یک عنصر تصادفی از هر یک از این لیست‌ها انتخاب شده و در داستان نهایی استفاده می‌شود
 #############################################################
 
 #some OUTPUT  from this program ==>
